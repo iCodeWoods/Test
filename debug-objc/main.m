6f7e9d27335e616c57bbf0f6bbe9724f0939ba21
iCodeWoods <iCodeWoods@gmail.com>
2019-04-10 13:07:30 +0800

    [runtime][load] 即使 + load 实现为空，仍然会在 load_images 时被加载

---
+++ debug-objc/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by Closure on 2018/12/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XXObject : NSObject
+@end
+
+@implementation XXObject
+
++ (void)load {
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
