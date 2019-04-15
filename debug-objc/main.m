1659491722684d8776bc088bb3d46c31d94f1f1f
iCodeWoods <iCodeWoods@gmail.com>
2019-04-15 14:12:00 +0800

    [runtime][initialize] 分类会覆盖类的 initialize，并且最后一个编译的分类的 initialize 才会生效

--- debug-objc/main.m
+++ debug-objc/main.m
@@ -28,23 +28,9 @@
 
 @implementation XXObjectSub
 
-//+ (void)initialize {
-////    [super initialize];
-//    NSLog(@"XXObjectSub initialize... %@", self);
-//}
-
-// 子类实现 initialize，调用 super
-//2019-04-15 13:53:04.821860+0800 debug-objc[20315:2950398] XXObject initialize... XXObject
-//2019-04-15 13:53:04.821936+0800 debug-objc[20315:2950398] XXObject initialize... XXObjectSub
-//2019-04-15 13:53:04.822003+0800 debug-objc[20315:2950398] XXObjectSub initialize... XXObjectSub
-
-// 子类实现 initialize，不调用 super
-//2019-04-15 13:53:32.698546+0800 debug-objc[20327:2950678] XXObject initialize... XXObject
-//2019-04-15 13:53:32.698601+0800 debug-objc[20327:2950678] XXObjectSub initialize... XXObjectSub
-
-// 子类没有实现 initialize
-//2019-04-15 13:54:40.554869+0800 debug-objc[20354:2951464] XXObject initialize... XXObject
-//2019-04-15 13:54:40.554933+0800 debug-objc[20354:2951464] XXObject initialize... XXObjectSub
++ (void)initialize {
+    NSLog(@"XXObjectSub initialize... %@", self);
+}
 
 @end
 
