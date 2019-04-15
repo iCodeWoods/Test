c4cb9a37017a2a1abe9cf01aa5894394e2113508
iCodeWoods <iCodeWoods@gmail.com>
2019-04-15 13:58:05 +0800

    [runtime][initialize] 子类的 initialize 如果调用父类，则父类的 initialize 会走两遍

--- debug-objc/main.m
+++ debug-objc/main.m
@@ -16,17 +16,42 @@
     NSLog(@"XXObject load...");
 }
 
++ (void)initialize {
+    NSLog(@"XXObject initialize... %@", self);
+}
+
+@end
+
+@interface XXObjectSub : XXObject
+
+@end
+
+@implementation XXObjectSub
+
+//+ (void)initialize {
+////    [super initialize];
+//    NSLog(@"XXObjectSub initialize... %@", self);
+//}
+
+// 子类实现 initialize，调用 super
+//2019-04-15 13:53:04.821860+0800 debug-objc[20315:2950398] XXObject initialize... XXObject
+//2019-04-15 13:53:04.821936+0800 debug-objc[20315:2950398] XXObject initialize... XXObjectSub
+//2019-04-15 13:53:04.822003+0800 debug-objc[20315:2950398] XXObjectSub initialize... XXObjectSub
+
+// 子类实现 initialize，不调用 super
+//2019-04-15 13:53:32.698546+0800 debug-objc[20327:2950678] XXObject initialize... XXObject
+//2019-04-15 13:53:32.698601+0800 debug-objc[20327:2950678] XXObjectSub initialize... XXObjectSub
+
+// 子类没有实现 initialize
+//2019-04-15 13:54:40.554869+0800 debug-objc[20354:2951464] XXObject initialize... XXObject
+//2019-04-15 13:54:40.554933+0800 debug-objc[20354:2951464] XXObject initialize... XXObjectSub
+
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        BOOL a = 8960;
-        if (a) {
-            NSLog(@"YES");
-        } else {
-            NSLog(@"NO");
-        }
-        NSLog(@"Hello, World!");
+        XXObjectSub *subXX = [[XXObjectSub alloc] init];
+        NSLog(@"subXX = %@", subXX);
     }
     return 0;
 }
